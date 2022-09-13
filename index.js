957b66314921065f774128abf1e1be8bd1a9b374
Lauren <l.cvengros@icloud.com>
2022-09-12 21:37:29 -0400

    added function to display prompt answers on the readme

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const inquirer = require('inquirer');
+
+const generateContent = ({ title, description, contents, installation, usage, license, contributors, tests, questions, gitHub, email}) =>
+ ` ## ${answers.title}
+
+ ## Description
+ ${answers.description}
+
+ ## Table of Contents
+ ${answers.contents}
+
+ ## Installation
+ ${answers.installation}
+
+ ## Usage
+ ${answers.usage}
+
+ ## License
+ ${answers.license}
+
+ ## Contributors
+ ${answers.contributors}
+
+ ## Tests
+ ${answers.tests}
+
+ ## Questions
+ ${answers.questions}
+
+ For any other questions, you may contact me via GitHub (https://github.com/${answers.gitHub})
+ or via email at ${answers.email}
+
+ 
+ 
+ 
+ 
+ `
