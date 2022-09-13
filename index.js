0b812169b162a9a6a2db31ed6abdbb86d7d19704
Lauren <l.cvengros@icloud.com>
2022-09-13 15:55:04 -0400

    added prompt questions

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const inquirer = require('inquirer');
 
-const generateContent = ({ title, description, contents, installation, usage, license, contributors, tests, questions, gitHub, email}) =>
+const generateContent = ({ title, description, contents, installation, usage, license, contributors, tests, gitHub, email}) =>
  ` ## ${answers.title}
 
  ## Description
  ${answers.description}
 
  ## Table of Contents
+ * [Installation] (#Installation)
+ * [Usage] (#Usage)
+ * [License] (#License)
+ * [Contributors] (#Contributors)
+ * [Tests] (#Tests)
+ * [Questions] (#Questions)
+ 
+
  ${answers.contents}
 
  ## Installation
@@ -25,13 +33,64 @@ const generateContent = ({ title, description, contents, installation, usage, li
  ${answers.tests}
 
  ## Questions
- ${answers.questions}
 
  For any other questions, you may contact me via GitHub (https://github.com/${answers.gitHub})
  or via email at ${answers.email}
 
- 
- 
- 
- 
  `
+
+inquirer
+.prompt([
+    {
+        type: "input",
+        name: "title",
+        Message: "Enter ther title of your project"
+    },
+    {
+        type: "input",
+        name: "description",
+        Message: "Enter a description for your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        Message: "describe the installation process of your application"
+    },
+    {
+        type: "input",
+        name: "usage",
+        Message: "What is your application used for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        Message: "Choose a license for your application",
+        choices: [
+            "Apache",
+            "Boost",
+            "GNU",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributors",
+        Message: "Who coontributed to this application?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        Message: "Enter any instructions for testing tyhis application"
+    },
+    {
+        type: "input",
+        name: "userName",
+        Message: "Enter your GitHib username"
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        Message: "Enter your email address for people to contact you"
+    },
+])
